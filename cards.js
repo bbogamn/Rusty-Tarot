b813b167a307fb9bcf7a257062d30a2eb5a39a12
bbogamn <valkobogdan55@gmail.com>
2025-05-25 01:40:18 +0200

    Add files via upload

--- cards.js
+++ cards.js
@@ -1,23 +1,35 @@
 const cards = [
   {
-    "title": "The Fool",
-    "translation": "Дурак",
-    "description_en": "Young and naive, taking the first step into the unknown by drinking a potion, unaware of consequences.",
-    "description_ru": "Молодой и наивный, делает первый шаг в неизвестность, пьёт зелье, не осознавая последствий.",
+    "title": {
+      "en": "The Fool",
+      "ru": "Дурак"
+    },
+    "description": {
+      "en": "Young and naive, taking the first step into the unknown by drinking a potion, unaware of consequences.",
+      "ru": "Молодой и наивный, делает первый шаг в неизвестность, пьёт зелье, не осознавая последствий."
+    },
     "image": "the_fool.jpg"
   },
   {
-    "title": "The Magician",
-    "translation": "Маг",
-    "description_en": "Manipulator of energy, transforming body and mind for the sake of power.",
-    "description_ru": "Манипулятор энергией, трансформирующий своё тело и разум ради власти.",
+    "title": {
+      "en": "The Magician",
+      "ru": "Маг"
+    },
+    "description": {
+      "en": "Manipulator of energy, transforming body and mind for the sake of power.",
+      "ru": "Манипулятор энергией, трансформирующий своё тело и разум ради власти."
+    },
     "image": "the_magician.jpg"
   },
   {
-    "title": "The High Priestess",
-    "translation": "Верховная Жрица",
-    "description_en": "Mysterious figure connected to secret knowledge and lunar intuition.",
-    "description_ru": "Таинственная фигура, связанная с тайными знаниями и лунной интуицией.",
+    "title": {
+      "en": "The High Priestess",
+      "ru": "Верховная Жрица"
+    },
+    "description": {
+      "en": "Mysterious figure connected to secret knowledge and lunar intuition.",
+      "ru": "Таинственная фигура, связанная с тайными знаниями и лунной интуицией."
+    },
     "image": "the_high_priestess.jpg"
   }
 ];
