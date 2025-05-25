5fdbaf6b8aa515664ff17fc89e69f7275672b66f
bbogamn <valkobogdan55@gmail.com>
2025-05-25 02:31:57 +0200

    Delete js directory

--- js/lang.js
+++
@@ -1,8 +0,0 @@
-
-function toggleLanguage() {
-    const elements = document.querySelectorAll('[data-lang-en]');
-    elements.forEach(el => {
-        const isEn = el.textContent === el.getAttribute('data-lang-en');
-        el.textContent = isEn ? el.getAttribute('data-lang-ru') : el.getAttribute('data-lang-en');
-    });
-}
